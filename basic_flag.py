96f1f1a42cb39506f75b852327ecc9988d8a57fb
Chris <cmulkey@olivetcollege.edu>
2012-11-05 18:03:24 -0500

    Attempt at basic Flag

---
+++ basic_flag.py
@@ -0,0 +1,20 @@
+# basic_flag.py
+# christopher mulkey
+# 11/05/2012
+# lab 1
+
+import pygame
+
+width = 480
+height = 600
+
+pygame.init()
+
+surface.fill(0, 0, 0) 		    # Black Backgroun
+
+pygame.draw.rect(0, 0, 104)     # White Flag Background
+pygame.draw.circle(0, 0, 255)   # Blue
+pygame.draw.circle(255, 255, 0)	# Yellow
+pygame.draw.circle(0, 0, 0)		# Black
+pygame.draw.circle(0, 255, 0)	# Green
+pygame.draw.circle(255, 0, 0)	# Red
