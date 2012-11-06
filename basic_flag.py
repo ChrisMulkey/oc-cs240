33e0be125ed875cb84b234a5c512b11be3e71e63
Chris <cmulkey@olivetcollege.edu>
2012-11-05 20:15:35 -0500

    Screen works
    Still need to figure out how to get the flag to be drawn. I am
    researching this, online for tips and ideas on making the code work.

--- basic_flag.py
+++ basic_flag.py
@@ -5,16 +5,23 @@
 
 import pygame
 
-width = 480
-height = 600
+width = 640
+height = 480
 
 pygame.init()
 
-surface.fill(0, 0, 0) 		    # Black Backgroun
+screen = pygame.display.set_mode((width, height)) # Screen
 
+
+screen.fill(0, 0, 0)            # Black Background
 pygame.draw.rect(0, 0, 104)     # White Flag Background
 pygame.draw.circle(0, 0, 255)   # Blue
 pygame.draw.circle(255, 255, 0)	# Yellow
-pygame.draw.circle(0, 0, 0)		# Black
+pygame.draw.circle(0, 0, 0)	# Black
 pygame.draw.circle(0, 255, 0)	# Green
 pygame.draw.circle(255, 0, 0)	# Red
+
+running = True
+for event in pygame.event.get():
+		if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key ==pygame.K_q : run = False
+
